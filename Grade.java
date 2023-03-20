38c5beb6502df4370034f041b63a1b04d9715bc2
mahmoud khaled <54672453+mahmoudkahled@users.noreply.github.com>
2023-03-20 18:52:58 +0200

    initial grade class

---
+++ Grade.java
@@ -0,0 +1,21 @@
+public class Grade {
+    private SubjectData subject_data;
+    private int GPA;
+    private String Grade;
+
+    public Grade(SubjectData subject_data){
+        this.GPA = 0;
+        this.Grade = "";
+        this.subject_data = subject_data;
+    }
+
+    public void calculate_gpa(){
+
+    }
+    public void calculate_grade(){
+
+    }
+    public void generate_student_gradings(){
+
+    }
+}
