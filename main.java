29e5007d6fba07e16515747333c0d2685a02a84d
mahmoud khaled <54672453+mahmoudkahled@users.noreply.github.com>
2023-04-01 00:21:17 +0200

    Edit main to send calculate_grade() function, the students marks

--- main.java
+++ main.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+
 public class main {
     public static void main(String[] args){
         String file_name = "src/Example Data.txt";
         SubjectData subject_data = new SubjectData();
         subject_data.load_data(file_name);
-        Grade grade = new Grade(subject_data);
-        grade.calculate_grade();
+        ArrayList<Integer> student_activities_marks = subject_data.getStudentActivitiesMarks();
+        ArrayList<Integer> oral_practical_marks = subject_data.getOralPracticalMarks();
+        ArrayList<Integer> midterm_exam_marks = subject_data.getMidtermExamMarks();
+        ArrayList<Integer> final_exam_marks = subject_data.getFinalExamMarks();
+        Grade grade = new Grade();
+        grade.calculate_grade(student_activities_marks, oral_practical_marks, midterm_exam_marks, final_exam_marks);
         grade.calculate_gpa();
         grade.generate_student_gradings();
     	
